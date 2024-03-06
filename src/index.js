44df1bea9df16ec22eb443afce3f1f0b4503cb1f
hemanthindala <hemanthindala2001@gmail.com>
2024-03-05 20:44:36 -0500

    made changes in index.js and added an env file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const {add} = require('./arithmetica')
 app.use(cors());
-const port = 3000;
 
+if (!process.env.PORT){
+    throw new Error('Please specify the port number for the HTTP server with the environment variable PORT.');
+}
+
+const port= process.env.PORT;
 // Define a route with parameters in the URL path
 app.get('/add/:num1/:num2', (req, res) => {
     // Extracting two input parameters from the URL path
@@ -22,7 +27,7 @@ app.get('/add/:num1/:num2', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.send('Arithmetic Service - last updated 3/4/2024')
 })
 
 app.listen(port, () => {
